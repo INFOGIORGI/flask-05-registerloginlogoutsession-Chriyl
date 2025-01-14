6a6e626c3bd14766f06529e617c80f1691ba38bb
Christian Montanaro <117461585+Chriyl@users.noreply.github.com>
2025-01-14 11:50:27 +0000

    pressoche finito ma non aggiunge al db

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, redirect # type: ignore
+from flask import Flask, render_template, redirect, request, url_for # type: ignore
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
+
+app.config["MYSQL_HOST"] = "138.41.20.102"
+app.config["MYSQL_PORT"] = 53306
+app.config["MYSQL_USER"] = "ospite"
+app.config["MYSQL_PASSWORD"] = "ospite"
+app.config["MYSQL_DB"] = "w3schools"
+
+Mysql= MySQL(app)
 
 @app.route("/")
 def home():
@@ -10,17 +20,62 @@ def home():
 def logout():
     return render_template("logout.html", titolo="logout", header="logout")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html", titolo="register", header="register")
+    if request.method == "GET":
+        return render_template("register.html", titolo="register", header="register")
+    elif request.method == "POST":
+        nome: str = request.form.get("nome")
+        username: str = request.form.get("username")
+        cognome: str = request.form.get("cognome")
+        pswd: str = request.form.get("pswd")
+        pswdconf: str = request.form.get("pswdconf")
+        print(nome, cognome, username, pswd, pswdconf)
 
-@app.route("/login")
+        if pswd != pswdconf:
+            return render_template("register.html", titolo="register", header="register", error="pswd non conformi")
+        
+        cursor: function = Mysql.connect.cursor()
+        query: str= """SELECT * FROM users WHERE username  = %s """
+        cursor.execute(query, (username,))
+        dati: tuple = cursor.fetchall()
+
+        print(dati)
+
+        if dati:
+            return render_template("register.html", titolo="register", header="register", error="utente gia esistente")
+
+        query_insert = """
+                INSERT INTO users (username, password, nome, cognome)
+                VALUES
+                (%s, %s, %s, %s)
+                """
+    
+        cursor.execute(query_insert, (username, pswd, nome, cognome,))
+        Mysql.connection.commit()
+       
+        return redirect(url_for("personal"))
+    
+
+
+        # eseguire query SELECT (email, pswd) FROM users WHERE users.email = email and users.pswd = pswd
+
+        # condizione per verificare se esiste gia 
+
+        # se esiste lancia errore al client
+
+        # se non esiste ritorna la sessione, inserisce nel db e redirecta su /personal
+            
+
+        
+
+@app.route("/login"  ,methods=["GET", "POST"])
 def login():
     return render_template("login.html", titolo="login", header="login")
 
 
 @app.route("/personal")
 def personal():
-    ...
+    return render_template("personal.html")
 
 app.run(debug=True)
